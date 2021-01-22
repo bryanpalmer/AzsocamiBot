8a343798eda77cc7a01f0b1f9d5bab5f48ec401d
Bryan Palmer <bpalmer@ramtechgroup.com>
2021-01-22 08:47:21 -0600

    Added armorType argument to lpc

--- bot/test.py
+++ bot/test.py
@@ -12,9 +12,19 @@ load_dotenv(
 )
 
 import wowapi
+import umj
 
+armors = wowapi.getLegendaryArmorsList()
+umjConn = umj.create_connection()
+for armorId in armors:
+    curId = armors[armorId]["id"]
+    print(f"Looking up armorId {armorId}")
+    item = umj.getItemById(umjConn, curId)
+    armors[armorId]["name"] = item.name
+    armors[armorId]["classname"] = item.classname
+    armors[armorId]["subclass"] = item.subclass
+    # print(vars(item))
+# print(armors)
+umjConn.close()
 
-ahdata = wowapi.getAuctionHouseData()
-
-with open("ahdata.json", "w") as outfile:
-    json.dump(ahdata, outfile)
+print(armors)
